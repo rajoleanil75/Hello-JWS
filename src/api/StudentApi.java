760c018b21029fe29a51e2ca6401de7ee97cc7ac
Mr.Anderson <rajoleanil75@gmail.com>
2019-10-19 11:58:41 +0530

    Show Student records in HTML tables using web service

--- src/api/StudentApi.java
+++ src/api/StudentApi.java
@@ -1,12 +1,9 @@
 package api;
 
-import DB.Student;
+import db.Student;
 
 import javax.jws.WebService;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,24 +17,31 @@ public class StudentApi {
     @Produces(MediaType.APPLICATION_JSON)
     public List getAllStudents()
     {
-        List<DB.Student>  studList = new ArrayList<>();
+        List<db.Student> studList = new ArrayList<>();
 
-        DB.Student s1 = new Student();
+        db.Student s1 = new Student();  //First Object
         s1.setRoll(1);
         s1.setName("ABC");
         studList.add(s1);
-
-        DB.Student s2 = new Student();
+        db.Student s2 = new Student();  //Second Object
         s2.setRoll(2);
         s2.setName("PQR");
         studList.add(s2);
-
-        DB.Student s3 = new Student();
+        db.Student s3 = new Student();  //Third Object
         s3.setRoll(3);
         s3.setName("XYZ");
         studList.add(s3);
 
         return studList;
+    }
+
+    @POST
+    @Path("addStudent")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int addStudent(@FormParam("student_roll")int roll, @FormParam("student_name") String name)
+    {
+        System.out.println("\nStudent Information : \n\tRoll No : "+roll+"\n\tName : "+name);
+        return 1;
     }
 
 }
