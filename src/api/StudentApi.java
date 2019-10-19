af38899190aa16d666a2853baabe642b286f1441
Mr.Anderson <rajoleanil75@gmail.com>
2019-10-19 12:04:19 +0530

    Referance and PPT added

--- src/api/StudentApi.java
+++ src/api/StudentApi.java
@@ -1,6 +1,6 @@
 package api;
 
-import db.Student;
+import DB.Student;
 
 import javax.jws.WebService;
 import javax.ws.rs.*;
@@ -17,17 +17,17 @@ public class StudentApi {
     @Produces(MediaType.APPLICATION_JSON)
     public List getAllStudents()
     {
-        List<db.Student> studList = new ArrayList<>();
+        List<DB.Student> studList = new ArrayList<>();
 
-        db.Student s1 = new Student();  //First Object
+        DB.Student s1 = new Student();  //First Object
         s1.setRoll(1);
         s1.setName("ABC");
         studList.add(s1);
-        db.Student s2 = new Student();  //Second Object
+        DB.Student s2 = new Student();  //Second Object
         s2.setRoll(2);
         s2.setName("PQR");
         studList.add(s2);
-        db.Student s3 = new Student();  //Third Object
+        DB.Student s3 = new Student();  //Third Object
         s3.setRoll(3);
         s3.setName("XYZ");
         studList.add(s3);
